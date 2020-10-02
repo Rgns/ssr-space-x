ede9fcc09fe407c4beb166aa9de5ca0e7948204a
rgns <guptaritu92@gmail.com>
2020-10-02 16:11:11 +0530

    integrated filter mission & card component

--- src/app/mission/space-filter.component.ts
+++ src/app/mission/space-filter.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-space-filter',
@@ -7,9 +7,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SpaceFilterComponent implements OnInit {
 
-  constructor() { }
+  @Input()
+  startYear = 2006;
+
+  @Input()
+  endYear = 2020;
+
+  _years: number[] = [];
+
+  constructor() {
+  }
 
   ngOnInit(): void {
+    let temp = this.startYear;
+    while (temp <= this.endYear) {
+      this._years.push(temp++);
+    }
+
   }
 
 }
